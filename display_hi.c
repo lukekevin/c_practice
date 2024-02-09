b4e68fd8b39346135fd7c00d304d873180a8ccd9
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2024-02-09 17:52:16 +0530

    Create display_hi.c

---
+++ display_hi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+	printf("H  \t H \t  I\n");
+	printf("H  \t H \t  I\n");
+	printf("H  \t H \t  I\n");
+	printf("H H H H H H \t  I\n");
+	printf("H  \t H \t  I\n");
+	printf("H  \t H \t  I\n");
+	printf("H  \t H \t  I\n");
+
+	return 0;
+
+
+
+
+
+
+}
