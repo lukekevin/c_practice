6f9c95fcb442fde376fb31139cd436b9111932b1
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2024-02-21 17:07:00 +0530

    Update simple_physics_functionbased.c

--- simple_physics_functionbased.c
+++ simple_physics_functionbased.c
@@ -50,29 +50,31 @@ void find_height(float *u, float g, float *s)
     printf("Height reached by object when thrown with u: %.2f (m/s) is %.2f (m)\n", *u, *s);
 }
 
-void kineticenergy(float *speed, float *m, float *K )
+float kineticenergy(float *speed, float *m, float *K )
 {
     char what_more_to_do[100];
-    printf("What more to do? find_v_from_K, find_K_from_v\n");
+    printf("In kinetic energy problem what more to do?\n \n");
+    printf("find_v_from_K, find_K_from_v ?\n");
     scanf("%s", what_more_to_do);
     if ( strcmp(what_more_to_do, "find_v_from_K")==0)
     {
-        printf("Val of K (J):\n");
+        printf("\nVal of K (J):\n");
         scanf("%f",K);
-        printf("Val of m (kg):\n");
+        printf("\nVal of m (kg):\n");
         scanf("%f",m);
         * speed=sqrt( 2 * (* K)/(* m));
-        printf("Speed is: %.2f",* speed);
+        printf("\nSpeed is: %.2f",* speed);
     }
     else if (strcmp(what_more_to_do, "find_K_from_v")==0)
     { 
-        printf("Val of u(m/s):\n");
+        printf("\nVal of u(m/s):\n");
         scanf("%f",speed);
-        printf("Val of m(kg):\n");
+        printf("\nVal of m(kg):\n");
         scanf("%f",m);
 
         * K=(0.5) * (* m) * (* speed) * (* speed);
-        printf("KE of object is : %.2f Joules.",* K);
+        printf("\nKE of object is : %.2f Joules.\n",* K);
+        return *K;
         
     }
 
@@ -80,16 +82,16 @@ void kineticenergy(float *speed, float *m, float *K )
 }
 
 
-void potential_energy(float *m, float *h, float *P, float g)
+float potential_energy(float *m, float *h, float *P, float g)
 {
     printf("Enter mass(kg):");
-    scanf("%f\n",m);
+    scanf("%f",m);
     printf("Enter height (m):");
-    scanf("%f\n",h);
+    scanf("%f",h);
 
     *P=(*m) *(* h)*g;
-    printf("PE is: %f Joules.",*P);
-
+    printf("PE is: %.2f Joules.",*P);
+    return *P;
 }
 
 int main()
@@ -102,7 +104,7 @@ int main()
 
     char what_to_do[100];
     printf("Which problem to do\n");
-    printf("Available problems:  find_speed,find_acceleration,find_height,kineticenergy,potentialenergy\n");
+    printf("Available problems:  find_speed,find_acceleration,find_height,kineticenergy,potentialenergy, totalenergy\n");
     printf("What to do:?\n");
     scanf("%s", what_to_do);
 
@@ -122,7 +124,7 @@ int main()
         find_height(&u, g, &s);
     }
 
-	else if ( strcmp(what_to_do, "find_kineticenergy")==0)
+	else if ( strcmp(what_to_do, "kineticenergy")==0)
 	{   
 
         
@@ -136,5 +138,15 @@ int main()
         potential_energy(&m,&h,&P,g);
     }
     
+    else if ( strcmp(what_to_do, "totalenergy")==0)
+    {
+        K=kineticenergy(&speed,&m,&K);
+        P=potential_energy(&m,&h,&P,g);
+        float TE=K+P;
+        printf("The TE of object is %.2f J",TE);
+    }
+    
     return 0;
+
 }
+// made changes in function data types from void to int so the return value can  be obtained for work in other functions
