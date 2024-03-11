5236f00d742a653dc7dbc95fa7d8fbd2cc15f592
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2024-03-11 12:37:41 +0530

    Update solar_system.c

--- solar_system.c
+++ solar_system.c
@@ -10,11 +10,13 @@ int main()
   float planet_mass [9] = {0.3e24, 4.87e24, 5.97e24, 0.64e24, 1.89e27, 5.68e26, 
   8.68e25, 1.02e26, 1.31e22 } ;
 
-  float planet_orbit [9] = {88, 225, 365, 687, 4333, 10747, 30589, 59800, 90560};
+  float planet_orbit [9] = {88, 225, 365, 687, 4333, 10747, 
+                              30589, 59800, 90560};
 
   float planet_no_moons [9] = {0,0,1,2,79,83,27,14,1};
 
-  float planet_radius [9] = {2440e3,6051e3,6371e3,3389e3,71492e3,60268e3,25559e3,24764e3,1186e3};
+  float planet_radius [9] = {2440e3,6051e3,6371e3,3389e3,71492e3,
+                            60268e3,25559e3,24764e3,1186e3};
 
   char planet_name[30];
   char what_to_do[30];
@@ -24,6 +26,8 @@ int main()
 
   float earth_mass=5.97e24;
   float earth_orbit_period=365;
+  float weight;   //weight of any object on a planet
+  float my_mass; //My mass
 
   int i; // iterable i
 
@@ -36,7 +40,7 @@ int main()
   }
 
   printf("Welcome to solar system data application \nHere you will various information for planets\n");
-  printf("What would you like to see? all_planet_masses, all_planet_orbits, specific_planet_data, planet_moons, all_planet_radius_smallg:\n");
+  printf("What would you like to see? \nall_planet_masses, \nall_planet_orbits, \nspecific_planet_data, \nplanet_moons, \nall_planet_radius_smallg, \nmy_weight:\n");
   scanf("%s", what_to_do);
   
   
@@ -45,7 +49,6 @@ int main()
   if ( strcmp (what_to_do,"all_planet_masses")==0)
   {
 
-  // Basic planet name displayer
   printf("Welcome to solar system data application \nHere you will various information for planets\n");  
  
   printf("\nMass data of planets\n");
@@ -106,6 +109,23 @@ else if ( strcmp(what_to_do,"specific_planet_data")==0)
       planets[i], planet_mass[i]/earth_mass, planet_orbit[i]/earth_orbit_period, planet_radius[i], g[i]);
     }
   }
+
 }
+
+//Weight calculator on different planets
+else if ( strcmp(what_to_do, "my_weight")==0)
+{
+  printf("Enter my mass:");
+  scanf("%f",&my_mass);
+
+  for (i=0;i<n;i++)
+  {
+    weight=my_mass*g[i];
+    printf("Weight is : %.2f N on %s\n", weight, planets[i]);
+
+  }
+  
+}
+
   return 0;
 }
