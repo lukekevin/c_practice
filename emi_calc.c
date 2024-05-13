2a17a3517935fd92069dcd7d88bc4c34c33d90dc
Kevin Luke <78250732+lukekevin@users.noreply.github.com>
2024-05-13 17:53:06 +0530

    Create emi_calc.c

---
+++ emi_calc.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main () 
+        {
+        float principal;
+        float years;
+        float interest_rate;
+        float emi;
+        char bank[20];
+
+
+        printf("Enter principal amount borrowed\n");
+        scanf("%f",&principal);
+
+        printf("Enter years of loan\n");
+        scanf("%f", &years);
+
+
+
+
+        printf("Enter initials of bank name:\n");
+        scanf("%s", bank);
+
+
+        if (strcmp(bank,"SBI")==0)
+        {
+
+        interest_rate=8.0;
+
+        }
+
+        else if (strcmp(bank,"ICICI")==0)
+        {
+
+        interest_rate=9.9;
+
+        }
+
+        else if (strcmp(bank,"HSBC")==0)
+        {
+
+        interest_rate=9.7;
+
+        }
+
+        else if (strcmp(bank,"ICICI")==0)
+        {
+
+        interest_rate=9.1;
+
+        }
+
+
+        float monthly_interest_rate= interest_rate/12/100; // in decimal values instead of percent
+                                                     
+
+        emi = principal * monthly_interest_rate * (pow(1+monthly_interest_rate,years)/ (pow(1+monthly_interest_rate,years)-1));
+
+        printf("The emi for %s bank is %.2f Rs",bank,emi);
+
+
+        return 0;
+
+        }
